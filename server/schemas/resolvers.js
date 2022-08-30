dc9aad83ca5c8a7cbfa9d656ef499700d4214926
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-08-30 14:19:32 -0400

    built and test queries for users nd thoughts

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,31 @@
 // functions connected to each query or mutation that perform the CRUD actions
 
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello, world!'
+        // GET all thoughts
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find().sort({ createdAt: -1 });
+        },
+        // GET single thought by id
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({ _id });
+        },
+        // GET all users
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
+        },
+        // GET single user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts');
         }
     }
 };
