7a8c08013d624af8fa596d5ca6b6da160edae5a0
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-08-31 11:35:30 -0400

    connect frontend client with backend server

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
+// integration Apollo inot the frontend of the app
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// connect the front end to the backend server's /graphql endpoint
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// returned code
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
