87fcba71950b01d67e4121c41b6ac785fbef9b77
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-01 18:17:26 -0400

    customize header based on loggedin status

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Header = () => {
+
+  // logout function and remove token
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <header className="bg-primary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -12,8 +20,20 @@ const Header = () => {
         </Link>
 
         <nav className='text-center'>
-          <Link to='/login'>Login</Link>
-          <Link to='/signup'>Signup</Link>
+          {/* check if logged in or not and display certain nav items */}
+          {Auth.loggedIn() ? (
+            <>
+              <Link to='/profile'>My profile</Link>
+              {/* logout will refresh and remove the token */}
+              <a href='/' onClick={logout}>Logout</a>
+            </>
+          ) : (
+            <>
+              <Link to='/login'>Login</Link>
+              <Link to='/signup'>Signup</Link>
+            </>
+          )
+          }
         </nav>
 
       </div>
