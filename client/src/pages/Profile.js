3222f0ca5b76d019ca6b892247d88c91b3b368b7
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-01 13:07:39 -0400

    render thoughts on profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,16 +1,41 @@
 import React from 'react';
 
+import { useParams } from 'react-router-dom';
+
+import ThoughtList from '../components/ThoughtList';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+
 const Profile = () => {
+
+  // grab the data
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { username: userParam }
+  });
+
+  const user = data?.user || [];
+
+  if (loading) {
+    return <div>L o a d i n g . . .</div>;
+  }
+
+  // render to the vdom
   return (
     <div>
       <div className="flex-row mb-3">
-        <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          {/* Viewing <usernames>'s profile. */}
+        <h2 className="text-primary bg-secondary radius p-3 display-inline-block">
+          Viewing {user.username}'s profile.
         </h2>
       </div>
 
       <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">{/* PRINT THOUGHT LIST  */}</div>
+        <div className="col-12 mb-3 col-lg-8">
+          {/* PRINT THOUGHT LIST  */}
+          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts . . .`} />
+        </div>
 
         <div className="col-12 col-lg-3 mb-3">{/* PRINT FRIEND LIST */}</div>
       </div>
