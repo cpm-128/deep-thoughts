c40122e726aeccc1922a24cc87162025b5ca3f56
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-01 11:24:55 -0400

    single thought query and rendering

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
 
 // singleThought functional component
 const SingleThought = props => {
 
+  // CREATING THE THOUGHT OBJ
   const { id: thoughtId } = useParams();
-  console.log('>> thoughtId >> ', thoughtId);
+    //console.log('>> thoughtId >> ', thoughtId);
+    const { loading, data } = useQuery(QUERY_THOUGHT, {
+      variables: { id: thoughtId }
+    });
+  // the data var will populate the thought obj
+  const thought = data?.thought || {};
+  // will display while data is being processed
+  if (loading) {
+    return <div>L o a d i n g . . .</div>;
+  }
 
+  // VDOM RENDERING
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header text-tertiary">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
+            {thought.username}
           </span>{' '}
-          thought on createdAt
+          thought on {thought.createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text.. n/a, hard coded</p>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
     </div>
